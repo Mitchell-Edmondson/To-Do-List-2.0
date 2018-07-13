a1c516269650eeb04f922e7f9903a352e3c8d325
miedmond <montycharlie2@hotmail.com>
2018-07-12 22:45:16 -0700

    More basic functionality

--- app/src/main/java/com/example/android/to_do_list_2_0/MainActivity.java
+++ app/src/main/java/com/example/android/to_do_list_2_0/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.to_do_list_2_0;
 
 import android.arch.lifecycle.ViewModelProviders;
 import android.arch.persistence.room.Room;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -33,19 +34,23 @@ public class MainActivity extends AppCompatActivity {
     public void putindb(View view)
     {
         EditText editText = findViewById(R.id.edit_text);
+        ConstraintLayout constraintLayout = findViewById(R.id.constraint_layout);
         Task task = new Task();
-        task.setId(0);
+        task.setId(constraintLayout.getChildCount() - 2);
         task.setUserTask(editText.getText().toString());
         viewModel.insert(task);
 
         List<Task> list = viewModel.readAll();
-        TextView textView = findViewById(R.id.text_view);
-        textView.setText(list.get(0).getUserTask());
+        updateText();
+
     }
 
-    public void updateText(List<Task> list){
+    public void updateText(){
         //Update screen
-        TextView textView = findViewById(R.id.text_view);
-        textView.setText(list.get(0).getUserTask());
+        ConstraintLayout constraintLayout = findViewById(R.id.constraint_layout);
+        TextView textView = new TextView(this);
+        constraintLayout.addView(textView);
+        List<Task> list = viewModel.readAll();
+        textView.setText(list.get(constraintLayout.getChildCount() - 3).getUserTask());
     }
 }
