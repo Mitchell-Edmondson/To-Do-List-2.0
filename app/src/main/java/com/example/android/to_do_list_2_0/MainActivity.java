5005d9b3d30be1561a0e23c821d4d0ef64610189
miedmond <montycharlie2@hotmail.com>
2018-07-12 17:33:31 -0700

    adding dependencies/setting up packages

--- app/src/main/java/com/example/android/to_do_list_2_0/MainActivity.java
+++ app/src/main/java/com/example/android/to_do_list_2_0/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.android.to_do_list_2_0;
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.android.to_do_list_2_0.ViewModel.ViewModel;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Creating the ViewModel
+        viewModel = ViewModelProviders.of(this).get(ViewModel.class);
     }
 }
---
+++ app/src/main/java/com/example/android/to_do_list_2_0/ViewModel/ViewModel.java
@@ -0,0 +1,12 @@
+package com.example.android.to_do_list_2_0.ViewModel;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.support.annotation.NonNull;
+
+public class ViewModel extends AndroidViewModel {
+
+    public ViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
