8b547d4eda87f5562424d5748d7b524f03c88ca7
miedmond <montycharlie2@hotmail.com>
2018-08-08 14:19:13 -0700

    Adding new test file

---
+++ app/src/test/java/com/example/android/to_do_list_2_0/activity_test.java
@@ -0,0 +1,24 @@
+package com.example.android.to_do_list_2_0;
+
+
+import com.example.android.to_do_list_2_0.Activities.addToDo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class)
+public class activity_test {
+
+    @Test
+    public void shouldNotBeNull(){
+        addToDo mainActivity = Robolectric.setupActivity(addToDo.class);
+        assertNotNull(mainActivity);
+    }
+
+}
