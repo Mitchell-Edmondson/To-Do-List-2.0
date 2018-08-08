736c890e786b525d860df09d5cf56ae4615e4b5a
miedmond <montycharlie2@hotmail.com>
2018-08-07 19:12:27 -0700

    Trying to fix lint issue with name of package

--- app/src/test/java/com/example/android/to_do_list_2_0/Activities_Test/MainActivityTest.java
+++ app/src/test/java/com/example/android/to_do_list_2_0/Activities_Test/MainActivityTest.java
@@ -1,8 +1,6 @@
 package com.example.android.to_do_list_2_0.Activities_Test;
 
 import com.example.android.to_do_list_2_0.BuildConfig;
-import com.example.android.to_do_list_2_0.activities.addToDo;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
