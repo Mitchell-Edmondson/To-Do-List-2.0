4a6cbb93b30f412fdd9cc7507fa094cdd78e7adc
miedmond <montycharlie2@hotmail.com>
2018-08-09 17:22:20 -0700

    Added activity check test for both activities

--- app/src/test/java/com/example/android/to_do_list_2_0/activity_test.java
+++ app/src/test/java/com/example/android/to_do_list_2_0/activity_test.java
@@ -1,6 +1,9 @@
 package com.example.android.to_do_list_2_0;
 
 
+import android.os.AsyncTask;
+
+import com.example.android.to_do_list_2_0.myActivities.MainActivity;
 import com.example.android.to_do_list_2_0.myActivities.addToDo;
 
 import org.junit.Test;
@@ -16,8 +19,23 @@ import static org.junit.Assert.assertNotNull;
 public class activity_test {
 
     @Test
-    public void shouldNotBeNull(){
-        addToDo mainActivity = Robolectric.setupActivity(addToDo.class);
+    public void addToDoShouldNotBeNull(){
+        addToDo myAddToDo = Robolectric.setupActivity(addToDo.class);
+        assertNotNull(myAddToDo);
+    }
+
+    @Test
+    public void mainActivityShouldNotBeNull(){
+        setUpMainActivity mainActivity = new setUpMainActivity();
         assertNotNull(mainActivity);
+    }
+
+    private static class setUpMainActivity extends AsyncTask<Void, Void, MainActivity>{
+
+        @Override
+        protected MainActivity doInBackground(Void... voids) {
+            MainActivity mainActivity = Robolectric.setupActivity(MainActivity.class);
+            return mainActivity;
+        }
     }
 }
